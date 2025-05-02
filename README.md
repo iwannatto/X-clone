493c17e16cc7cd8cb642bb2c16e6e77308d10d89
iwannatto <iwannatto@gmail.com>
2025-05-02 12:24:58 +0000

    Hello world

---
+++ README.md
@@ -0,0 +1,8 @@
+devcontainer を使っている
+
+# ローカル起動
+
+devcontainer で開く
+frontend 下で`npm run dev`  
+backend 下で`npm run start:dev`
+http://localhost:3010/ で Hello World
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: 'http://localhost:3010', // Reactのフロントエンドからのリクエストを許可
+    methods: 'GET,POST,PUT,DELETE', // 許可するHTTPメソッド
+    allowedHeaders: 'Content-Type, Authorization', // 許可するヘッダー
+  });
+
+  await app.listen(3011);
 }
 bootstrap();
