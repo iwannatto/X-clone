5e583ce9fac322902818c10af77035001923f3d6
iwannatto <iwannatto@gmail.com>
2025-05-04 13:01:33 +0000

    ConfigModuleで環境変数を読み込むようにした

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigModule } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,11 @@ async function bootstrap() {
     origin: 'http://localhost:3010', // Reactのフロントエンドからのリクエストを許可
     methods: 'GET,POST,PUT,DELETE', // 許可するHTTPメソッド
     allowedHeaders: 'Content-Type, Authorization', // 許可するヘッダー
+  });
+
+  ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: `.env.${process.env.NODE_ENV}`,
   });
 
   await app.listen(3011);
